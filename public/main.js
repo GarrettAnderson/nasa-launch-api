56b695c2671f8e0935b67af1dd96034cf6b6494b
GarrettAnderson <garrettleegrahamanderson@gmail.com>
2019-02-15 12:07:12 -0500

    added to JS in order to get the NASA image of the day to populate dynamically

--- public/main.js
+++ public/main.js
@@ -1,12 +1,19 @@
+let imageURL = []
+
 const main = () => {
   getNasaImage()
   getLauchDetail()
 }
 
 const getNasaImage = () => {
-  fetch(`https://sdg-astro-api.herokuapp.com/api/Nasa/apod`)
-    .then((resp) => resp.json())
-    .then((image) => console.log(image))
+  fetch(`https://sdg-astro-api.herokuapp.com/api/Nasa/apod`).then((resp) => resp.json()).then((image) => {
+    console.log(image)
+    imageURL = image.url
+    console.log(imageURL)
+    let hero = document.querySelector('.hero-image')
+    console.log(hero)
+    hero.style.background = " url('" + `${imageURL}` + "') "
+  })
 }
 
 const getLauchDetail = () => {
